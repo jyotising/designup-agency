853e68023ee177c799bdae7052497b8500a921c4
Jyoti Sing <jyotising@Jyotis-MacBook-Pro.local>
2022-11-24 23:40:43 +0530

    mini update

--- src/App.js
+++ src/App.js
@@ -11,11 +11,11 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://jyoti.design"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Contact us
         </a>
       </header>
     </div>
